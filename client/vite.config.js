f950813c12cdd81741fda747eba0fdf91a0badb8
Slavi Sokolov <leaguebro747@gmail.com>
2025-04-14 03:33:07 -0700

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/', // Adjust this if deployed under a subdirectory
+  plugins: [react()]
+});
